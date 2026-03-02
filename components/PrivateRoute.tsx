191a822cf25c7ccd74b95912c91878a363d369e8
Lamarck Carregosa <lamarckcarregosa@gmail.com>
2026-03-02 15:51:16 -0300

    Update PrivateRoute.tsx

--- components/PrivateRoute.tsx
+++ components/PrivateRoute.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import { useAuth } from "../../context/authcontext";
+import { useAuth } from "../context/authcontext";
 
 export default function PrivateRoute({ children }: any){
 
